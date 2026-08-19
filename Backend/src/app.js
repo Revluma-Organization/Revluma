204620a1d1315525e6d98835dc63b38e495908bd
Revluma <splendorbenjamin4@gmail.com>
2026-08-19 14:42:05 +0100

    Making changes to app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -18,6 +18,7 @@ const eventRoutes = require("./route/eventRoute");
 const preferencesRoutes = require("./route/preferencesRoute");
 const sessionRoutes = require("./route/SessionRoute");
 const settingsRoutes = require("./route/settingsRoute");
+const subscriptionRoutes = require("./route/subscriptionRoute");
 
 
 
@@ -83,6 +84,7 @@ app.use('/api/v1/events', eventRoutes);
 app.use("/api/v1/preferences", preferencesRoutes);
 app.use("/api/v1/auth/",sessionRoutes);
 app.use("/api/v1/settings",settingsRoutes);
+app.use("/api/v1/subscriptions", subscriptionRoutes);
 
 
 // ── Health check
